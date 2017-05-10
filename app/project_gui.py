c1a558ff0ec161abe2dd52e2d145de8b597f2420
Borde <Bordee@labcorp.com>
2017-05-10 12:24:21 -0400

    Able to read icd file and print output on console

--- app/project_gui.py
+++ app/project_gui.py
@@ -1,86 +1,85 @@
 from PyQt4 import QtGui, QtCore
 from functools import partial
 
-from read_xml import Xml_File_Parser
+from read_xml import XmlFileParser
+
 
 class Window(QtGui.QMainWindow):
-	def __init__(self, filename= None, parent=None):
-		super(Window, self).__init__(parent)
-		self.filename_list = filename
-		self.setGeometry(300, 150, 500, 400)
-		self.setWindowIcon(QtGui.QIcon("querybots.png"))
-		self.setWindowTitle("QryBots")
-		# self.resize(500,400)
-		self.widget = QtGui.QWidget()
+    def __init__(self, filename=None, parent=None):
+        super(Window, self).__init__(parent)
+        self.filename_list = filename
+        self.setGeometry(300, 150, 500, 400)
+        self.setWindowIcon(QtGui.QIcon("querybots.png"))
+        self.setWindowTitle("QryBots")
+        # self.resize(500,400)
+        self.widget = QtGui.QWidget()
 
-		self.label = QtGui.QLabel("Please click the button below to run the Query:", self)
-		self.hbox = QtGui.QHBoxLayout()
-		self.hbox.addWidget(self.label)
-		# self.hbox.addWidget(self.dateEdit)
-		self.blank = QtGui.QVBoxLayout()
-		self.label1 = QtGui.QLabel("")
-		self.label1.setFixedHeight(50)
-		self.blank.addWidget(self.label1)
-		self.vbox = QtGui.QVBoxLayout()
-		self.vbox.setSpacing(10)
-		self.label.setFixedWidth(500)
-		self.font = QtGui.QFont()
-		self.font.setPointSize(14)
-		self.font.setBold(True)
-		self.font.setWeight(100)
-		self.font.setFamily("Helvetica")
-		self.palette = QtGui.QPalette()
-		# palette.setColor(QtGui.QPalette.Foreground, QtCore.Qt.white)
-		self.label.setPalette(self.palette)
-		self.label.setFont(self.font)
-		self.label.setPalette(self.palette)
-		self.label.setFont(self.font)
-		self.grid = QtGui.QGridLayout()
-		self.grid.setHorizontalSpacing(100)
-		self.grid.setVerticalSpacing(20)
-		self.dynamically_generated_buttons()
-		self.vbox.addLayout(self.hbox)
-		self.vbox.addLayout(self.blank)
-		self.vbox.addLayout(self.grid)
-		self.vbox.setAlignment(QtCore.Qt.AlignTop)
-		self.widget.setLayout(self.vbox)
+        self.label = QtGui.QLabel("Please click the button below to run the Query:", self)
+        self.hbox = QtGui.QHBoxLayout()
+        self.hbox.addWidget(self.label)
+        # self.hbox.addWidget(self.dateEdit)
+        self.blank = QtGui.QVBoxLayout()
+        self.label1 = QtGui.QLabel("")
+        self.label1.setFixedHeight(50)
+        self.blank.addWidget(self.label1)
+        self.vbox = QtGui.QVBoxLayout()
+        self.vbox.setSpacing(10)
+        self.label.setFixedWidth(500)
+        self.font = QtGui.QFont()
+        self.font.setPointSize(14)
+        self.font.setBold(True)
+        self.font.setWeight(100)
+        self.font.setFamily("Helvetica")
+        self.palette = QtGui.QPalette()
+        # palette.setColor(QtGui.QPalette.Foreground, QtCore.Qt.white)
+        self.label.setPalette(self.palette)
+        self.label.setFont(self.font)
+        self.label.setPalette(self.palette)
+        self.label.setFont(self.font)
+        self.grid = QtGui.QGridLayout()
+        self.grid.setHorizontalSpacing(100)
+        self.grid.setVerticalSpacing(20)
+        self.dynamically_generated_buttons()
+        self.vbox.addLayout(self.hbox)
+        self.vbox.addLayout(self.blank)
+        self.vbox.addLayout(self.grid)
+        self.vbox.setAlignment(QtCore.Qt.AlignTop)
+        self.widget.setLayout(self.vbox)
+        self.setCentralWidget(self.widget)
+        self.show()
 
-		self.setCentralWidget(self.widget)
-		self.show()
+    def dynamically_generated_buttons(self):
+        self.font1 = QtGui.QFont()
+        self.font1.setPointSize(13)
+        self.font1.setBold(True)
+        self.font1.setWeight(50)
+        self.font1.setFamily("Helvetica")
+        self.palette1 = QtGui.QPalette()
+        positions = [(i, j) for i in range(len(self.filename_list)) for j in range(2)]
+        for position, name in zip(positions, self.filename_list):
+            if name == '':
+                continue
+            self.button = QtGui.QPushButton(name)
+            self.button.clicked.connect(partial(self.get_attrib_values, self.button.text()))
+            self.button.setFixedHeight(40)
+            self.button.setPalette(self.palette1)
+            self.button.setFont(self.font1)
+            self.grid.addWidget(self.button, *position)
 
-	def dynamically_generated_buttons(self):
-		self.font1 = QtGui.QFont()
-		self.font1.setPointSize(13)
-		self.font1.setBold(True)
-		self.font1.setWeight(50)
-		self.font1.setFamily("Helvetica")
-		self.palette1 = QtGui.QPalette()
-		positions = [(i,j) for i in range(len(self.filename_list)) for j in range(2)]
-		for position, name in zip(positions, self.filename_list):
-			if name == '':
-				continue
-			self.button = QtGui.QPushButton(name)
-			self.button.clicked.connect(partial(self.get_attrib_values, self.button.text()))
-			self.button.setFixedHeight(40)
-			self.button.setPalette(self.palette1)
-			self.button.setFont(self.font1)
-			self.grid.addWidget(self.button, *position)
+    def get_attrib_values(self, text):
 
-
-	def get_attrib_values(self, text):
-
-		xml_file = str(text)+'.xml'
-		xml_instance.fetch_file_data(xml_file)
+        xml_file = str(text) + '.xml'
+        xml_instance.fetch_file_data(xml_file)
 
 
 if __name__ == "__main__":
-	import os
-	import sys
+    import os
+    import sys
 
-	app = QtGui.QApplication(sys.argv)
-	BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-	path = os.path.join(BASE_DIR, "config")
-	xml_instance = Xml_File_Parser(path)
-	filename = xml_instance.get_filename()
-	window = Window(filename=filename)
-	sys.exit(app.exec_())
+    app = QtGui.QApplication(sys.argv)
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    path = os.path.join(BASE_DIR, "config")
+    xml_instance = XmlFileParser(path)
+    filename = xml_instance.get_filename()
+    window = Window(filename=filename)
+    sys.exit(app.exec_())
--- app/read_xml.py
+++ app/read_xml.py
@@ -1,41 +1,53 @@
 import os
 import pandas as pd
+from tabulate import tabulate
 import xml.etree.ElementTree as ET
 
 from query_dialog import Query_Window
 
 
-class Xml_File_Parser(object):
-	def __init__(self, path):
-		self.path = path
+class XmlFileParser(object):
+    def __init__(self, path):
+        self.path = path
 
-	def get_filename(self):
-		filename_list = []
-		for files in os.listdir(self.path):
-			if ".xml" in files:
-				name, ext = files.split('.')
-				filename_list.append(name)
+    def get_filename(self):
+        filename_list = []
+        for files in os.listdir(self.path):
+            if ".xml" in files:
+                name, ext = files.split('.')
+                filename_list.append(name)
 
-		return filename_list
+        return filename_list
 
-	def fetch_file_data(self, file_name):
-		fetched_file =  os.path.join(self.path, file_name)
-		tree = ET.parse(fetched_file)
-		root = tree.getroot()
-		description = root.get('desc')
-		field_name = []
-		field_type = []
-		# x = root.get('sql')
-		for child in root.iter('input'):
-			name = child.get('name')
-			type = child.get('type')
-			field_name.append(name)
-			field_type.append(type)
-		df = pd.DataFrame(data=field_type, index= field_name, columns=['Type'])
-		# print df
-		self.wind = Query_Window(fields=df, desc = description)
-		self.wind.show()
+    def fetch_file_data(self, file_name):
+        fetched_file = os.path.join(self.path, file_name)
+        tree = ET.parse(fetched_file)
+        root = tree.getroot()
+        description = root.get('desc')
+        field_name = []
+        field_type = []
+        field_ids = []
+        # x = root.get('sql')
+        for child in root.iter('input'):
+            name = child.get('name')
+            type = child.get('type')
+            field_id = child.get('id')
+            tab = child.get('table')
+            if tab:
+                print "-------------------------------"
+                print "|    Input Table: ", tab,    '|'
+                print "-------------------------------"
 
+            field_name.append(name)
+            field_type.append(type)
+            field_ids.append(field_id)
+        df = pd.DataFrame({'Type': field_type, 'Id': field_ids}, index=field_name)
+        print tabulate(df, headers='keys', tablefmt='psql')
+        for chi in root.iter('tables'):
+            for t in root.iter('table'):
+                print t.get('name')
+        self.wind = Query_Window(fields=df, desc=description)
+        self.wind.show()
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # path = os.path.join(BASE_DIR, "config")
